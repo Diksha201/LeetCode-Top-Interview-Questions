ff122d5eb2a669759aa9c00b3cc43eb1f2cf625c
Diksha Kumari Singh <130738814+Diksha201@users.noreply.github.com>
2025-05-18 10:09:41 +0530

    Add files via upload

---
+++ Arrays And String/Remove Duplicate From Sorted Array 2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+         if (nums.size() <= 2) return nums.size(); 
+    
+    int i = 1; 
+    int j=2;
+
+    for (int j = 2; j < nums.size(); j++) {
+        if (nums[j] != nums[i - 1]) { 
+           
+            i++;
+             nums[i] = nums[j];
+            
+        }
+    }
+   
+return i + 1; 
+ }
+};
